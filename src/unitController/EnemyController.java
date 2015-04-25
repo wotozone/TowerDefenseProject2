24c766d2f73eaa85eecc2e781f8425ae3fdb83ea
wotozone <xoduszzong@gmail.com>
2015-04-24 20:36:15 -0700

    working on enemy part 2

--- src/unitController/EnemyController.java
+++ src/unitController/EnemyController.java
@@ -13,6 +13,13 @@ public class EnemyController {
     
     public static final int MAX_ENEMY=300;
     
+    public EnemyInfo info = new EnemyInfo();
+    
+    public EnemyData enemyData = null;
+    
+    public int enemyLevel = 0;//Enemy level starts as 1
+    public int enemyAmount = 0;//How many enemy will be loaded
+    
     public EnemyController(){
         initEnemyData();
     }
@@ -21,6 +28,23 @@ public class EnemyController {
         for(int i=0;i<MAX_ENEMY;i++){
             EnemyData.enemy[i] = new EnemyData();
         }
+    }
+    
+    private void getNextLevelEnemy(){
+        enemyLevel++;
+        enemyData= info.GetEnemyData(enemyLevel);
+        
+    }
+    
+    private void loadEnemyInField(){
+        if(enemyAmount>MAX_ENEMY)enemyAmount=MAX_ENEMY;
+        for(int i=0;i<enemyAmount;i++){
+            EnemyData.enemy[i]=enemyData;
+        }
+    }
+    
+    private void setEnemyLevel(int lv){//Use only for test or unfindable bugs
+        enemyLevel=lv;
     }
     
 }
